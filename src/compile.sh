a40fbc81c346bcc46df122883ffef1ac1cdee425
Pavel <yakomazov.pavel@yandex.ru>
2023-12-30 03:15:16 +0300

    change compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sdcc --stack-auto --model-large -mmcs51 --opt-code-size --code-loc 0x2100 "./labs/7 Noise/noize.c" \
 -o "/home/pavel/hobby/SDK/.temp/"  \
-&& packihx "/home/pavel/hobby/SDK/.temp/noize.ihx" > "/home/pavel/hobby/SDK/hexs/7noize.hex"
+&& packihx "/home/pavel/hobby/SDK/.temp/noize.ihx" > "/home/pavel/hobby/SDK/hexs/7noize.hex" \
+&& echo ":022100060000D7" >> "/home/pavel/hobby/SDK/hexs/7noize.hex"
+
 # --xstack --xstack-loc 0x3000 
